af3e06e354686bd64f70e36b4acc73cf0ab8f2a3
Soc Virnyl Estela <uncomfy@uncomfyhalomacro.pl>
2023-10-24 16:38:29 +0800

    initial layout skeleton for libotap
    Signed-off-by: Soc Virnyl Estela <uncomfy@uncomfyhalomacro.pl>

--- libotap/src/consts.rs
+++ libotap/src/consts.rs
@@ -6,5 +6,5 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-pub const API_URL: &str = "https://api.opensuse.org/attribute";
+pub const API_URL: &str = "https://api.opensuse.org/";
 pub const API_HEADER: &str = "Accept: application/xml; charset=utf-8";
--- libotap/src/lib.rs
+++ libotap/src/lib.rs
@@ -6,7 +6,24 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-pub mod consts;
+pub mod announcements;
+pub mod attributes;
+pub mod build;
+pub mod comments;
+pub mod configuration;
+pub mod distribution;
+pub mod general_information;
+pub mod groups;
+pub mod issue_trackers;
+pub mod notifications;
+pub mod person;
+pub mod published;
+pub mod requests;
+pub mod search;
+pub mod sources;
+pub mod staging;
+pub mod status;
 
+pub mod consts;
 pub use crate::consts::API_HEADER;
 pub use crate::consts::API_URL;
