95a6fa385f40f8f43f30de8aef5e2e9a695388b9
Pi-Cla <pirateclip@protonmail.com>
2024-05-15 16:31:27 -0600

    refactor: cleanup get_services()

--- libotap/src/general_information/service.rs
+++ libotap/src/general_information/service.rs
@@ -2,19 +2,24 @@
 
 // Copyright Gordon Leung
 
-use reqwest::Error;
+use reqwest::Client;
 use url::Url;
 
 use crate::consts::API_URL;
 
-
 #[tokio::main]
-pub async fn get_services() -> Result<(), Error> {
+pub async fn get_services() -> Result<(), reqwest::Error> {
     // This should always construct a valid URL
     #[allow(clippy::unwrap_used)]
     let url = Url::parse(&(API_URL.to_string() + "service")).unwrap();
 
-    let resp = reqwest::get(url).await?.text().await?;
+    let resp = Client::new()
+        .get(url)
+        .header("accept", "application/xml; charset=utf-8")
+        .send()
+        .await?
+        .text()
+        .await?;
 
-    Ok(println!("{}",resp))
+    Ok(println!("{}", resp))
 }
